f56edb9891848ab737d88c82b89874bd87ac15e8
Crystal0108W <crystal0108wong@gmail.com>
2017-04-16 17:18:21 -0500

    Update Physical Optimizer_DataLoader.py

--- Database Systems CS386D/Physical Optimizer_DataLoader.py
+++ Database Systems CS386D/Physical Optimizer_DataLoader.py
@@ -6,69 +6,65 @@ import sqlite3
 conn = sqlite3.connect('/Users/yw7986/Desktop/my_db')
 c = conn.cursor()
 
+stats_a = []
+stats_b = []
 # generate data
+def generator_a():
+	''' GENERATE DATA '''
+	stats = []
+	index = 0
+	while len(stats) < 5000000:
+		# random 8 digits string
+		str = ''.join([random.choice(string.ascii_lowercase) for i in range(8)])
+		t = (index, random.randint(1, 5000000), random.randint(1, 5000000), str)
+		stats.append(t)
+		index +=1
 
-def generator():
+	# more efficient
+	# stats_a = [(i, random.randint(1, 5000000), random.randint(1, 5000000), str) for i in range(5000001)]
+	return stats
+
+#stats_a = generator_a()
+
+def generator_b():
 	''' GENERATE DATA '''
 	ht = [i for i in range(0, 100000)] * 10
 	tt = [i for i in range(0, 10000)]  * 100
 	ot = [i for i in range(0, 1000)]   * 1000
 	str = ''.join([random.choice(string.ascii_lowercase) for i in range(8)])
-	stats = [(i + 1, ht[i], tt[i], ot[i], str) for i in range(0, 1000000)]
+	stats = [(i + 1, ht[i], tt[i], ot[i], str) for i in range(0, 100000)]
 	return stats
 
-#stats = generator()
-stats = []
-#print(stats)
-print(len(stats))
+#stats_b = generator_b
 
 # Create Tables
-c.execute('''CREATE TABLE IF NOT EXISTS A(
-				pk NUMBER PRIMARY KEY,
-				ht NUMBER,
-				tt NUMBER,
-				ot NUMBER,
-				filler CHAR(247), 
-			)''')
-
-c.execute('''CREATE TABLE IF NOT EXISTS B(
-				pk NUMBER PRIMARY KEY,
-				ht NUMBER,
-				tt NUMBER,
-				ot NUMBER,
-				filler CHAR(247), 
+c.execute('''CREATE TABLE IF NOT EXISTS benchmark(
+				theKey NUMBER PRIMARY KEY,
+				columnA NUMBER,
+				columnB NUMBER,
+				filler CHAR(247)
 			 )''')
 
-c.execute('''CREATE TABLE IF NOT EXISTS C(
-				pk NUMBER PRIMARY KEY,
-				ht NUMBER,
-				tt NUMBER,
-				ot NUMBER,
-				filler CHAR(247), 
+c.execute('''CREATE TABLE IF NOT EXISTS benchmark_rand(
+				theKey NUMBER PRIMARY KEY,
+				columnA NUMBER,
+				columnB NUMBER,
+				filler CHAR(247)
 			 )''')
 
-# Clear previous data; Insert stats into A
-c.execute('DELETE FROM A')
+# Clear previous data; Insert stats into benchmark
+c.execute('DELETE FROM benchmark')
 c.execute('VACUUM')
-c.executemany('INSERT INTO A(pk, ht, tt, ot, filler) VALUES (?,?,?,?)', stats)
-
-c.execute('DELETE FROM B')
-c.execute('VACUUM')
-c.executemany('INSERT INTO B(pk, ht, tt, ot, filler) VALUES (?,?,?,?)', stats)
-
-c.execute('DELETE FROM C')
-c.execute('VACUUM')
-c.executemany('INSERT INTO B(pk, ht, tt, ot, filler) VALUES (?,?,?,?)', stats)
-
+c.executemany('INSERT INTO benchmark(theKey, columnA, columnB, filler) VALUES (?,?,?,?)', stats_a)
 
 
 # Shuffle stats
-# Clear previous data; Insert stats into A
-#random.shuffle(stats)
-#c.execute('DELETE FROM benchmark_rand')
-#c.execute('VACUUM')
-#c.executemany('INSERT INTO benchmark_rand(theKey, columnA, columnB, filler) VALUES (?,?,?,?)', stats)
+# Clear previous data; Insert stats into benchmark
+random.shuffle(stats_a)
+c.execute('DELETE FROM benchmark_rand')
+c.execute('VACUUM')
+c.executemany('INSERT INTO benchmark_rand(theKey, columnA, columnB, filler) VALUES (?,?,?,?)', stats_a)
 
 # Save execute and close connection
 conn.commit()
-conn.close()
+conn.close()                                                                                                                                                                                                  
